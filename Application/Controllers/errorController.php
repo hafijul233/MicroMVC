0534fe7e50ae95e8e07805f581514a6841672912
Hafijul Islam <hafijul233@gmail.com>
2019-07-02 17:02:15 +0600

    $url variable fix

---
+++ Application/Controllers/errorController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of errorController
+ *
+ * @author hafiz
+ */
+class errorController {
+    function __construct() {
+        echo 'page No found';
+    }
+}
--- Application/Core/Application.php
+++ Application/Core/Application.php
@@ -1,30 +1,39 @@
 <?php
 
-class Application {
+class Application
+{
 
-    protected $controller = 'Home';
-    protected $method = 'index';
+    protected $controller = 'homeController'; //Default Controller for all only Method
+    protected $method = 'index'; //Default Method for all Controller
     protected $params = [];
 
-    public function __construct() {
+    public function __construct() 
+    {
         $routes = $this->parseUrl();
-        print_r($routes);
         
-//echo 'This is a Application Instance';
+        if(file_exists('../Application/Controllers/' . $routes[0] . '.php') == TRUE) 
+        {
+            $this->controller = $route[0];
+        }
+        else 
+        {
+            $this->controller = 'errorController';
+        }
+        
+        require_once '../Application/Controllers/' . $this->controller . '.php';
     }
 
-    public function parseUrl() {
+    public function parseUrl() 
+    {
         $url = NULL;
-        if (isset($_GET['url'])) {
-            
+        if (isset($_GET['url']))
+        {
             $url = rtrim($_GET['url'], '/'); //Removes whitespace or other predefined characters from the right side of a string
             $url = filter_var($url, FILTER_SANITIZE_URL); //FILTER_SANITIZE_URL filter removes all illegal URL characters from a string
             $url = explode('/', $url); //substring the url into string array with / as seperator
             
             return $url;
         }
-        //lunch the 404 error working
-        return 'Nothing Found in Parseurl';
     }
 
 }
